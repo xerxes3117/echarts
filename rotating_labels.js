cbceca617c58d331d154eb08e140f25f32820af2
vaibhav sharma <vsnsit@gmail.com>
2019-01-08 21:03:15 +0530

    Update rotating_labels.js

--- rotating_labels.js
+++ rotating_labels.js
@@ -1,38 +1,26 @@
+//Issue: Doesn't work for datapoints less than 20
+//Another separate possible solution: rotate labels based on number of datapoints and labels length
+
 var data1 = ['Campaign 1'
         , 'Campaign 2'
         , 'Campaign 3'
         , 'Campaign 4'
         , 'Campaign 5'
-        
         , 'Campaign 6'
         , 'Campaign 7'
-        
         , 'Campaign 8'
-        
         , 'Campaign 9'
-        
         , 'Campaign 10'
-        
         , 'Campaign 11'
-        
         , 'Campaign 12'
-        
         , 'Campaign 13'
-        
-        , 'Campaign 14'
-        
+        , 'Campaign 14'  
         , 'Campaign 15'
-        
         , 'Campaign 16'
-        
         , 'Campaign 17'
-        
         , 'Campaign 18'
-        
         , 'Campaign 19'
-        
         , 'Campaign 20'
-        
         /**/
         ]
 
