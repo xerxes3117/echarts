58f25b783fffb7f827c383214556f2c3aa7ec674
vaibhav sharma <vsnsit@gmail.com>
2019-01-16 19:40:04 +0530

    Create treemap_data.js

---
+++ treemap_data.js
@@ -0,0 +1,637 @@
+data = [
+  {
+    "SUM_applications": "6100",
+    "campaign": "Campaign 1",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "8100",
+    "campaign": "Campaign 1",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "5600",
+    "campaign": "Campaign 1",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "4400",
+    "campaign": "Campaign 1",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "3200",
+    "campaign": "Campaign 1",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "100",
+    "campaign": "Campaign 1",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "1900",
+    "campaign": "Campaign 1",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 1",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 1",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 1",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 1",
+    "fico_band": "800-819"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 1",
+    "fico_band": "820-839"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 1",
+    "fico_band": "840-859"
+  },
+  {
+    "SUM_applications": "6500",
+    "campaign": "Campaign 2",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "8500",
+    "campaign": "Campaign 2",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "6600",
+    "campaign": "Campaign 2",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "5300",
+    "campaign": "Campaign 2",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "3000",
+    "campaign": "Campaign 2",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "200",
+    "campaign": "Campaign 2",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "1900",
+    "campaign": "Campaign 2",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 2",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 2",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 2",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 2",
+    "fico_band": "800-819"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 2",
+    "fico_band": "820-839"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 2",
+    "fico_band": "840-859"
+  },
+  {
+    "SUM_applications": "7500",
+    "campaign": "Campaign 3",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "8600",
+    "campaign": "Campaign 3",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "4500",
+    "campaign": "Campaign 3",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "3300",
+    "campaign": "Campaign 3",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "2100",
+    "campaign": "Campaign 3",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "500",
+    "campaign": "Campaign 3",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "2400",
+    "campaign": "Campaign 3",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "300",
+    "campaign": "Campaign 3",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "100",
+    "campaign": "Campaign 3",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 3",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "9900",
+    "campaign": "Campaign 4",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "12100",
+    "campaign": "Campaign 4",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "6800",
+    "campaign": "Campaign 4",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "5500",
+    "campaign": "Campaign 4",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "3300",
+    "campaign": "Campaign 4",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "500",
+    "campaign": "Campaign 4",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "2800",
+    "campaign": "Campaign 4",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "300",
+    "campaign": "Campaign 4",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "100",
+    "campaign": "Campaign 4",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 4",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "11500",
+    "campaign": "Campaign 5",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "13500",
+    "campaign": "Campaign 5",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "8000",
+    "campaign": "Campaign 5",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "6100",
+    "campaign": "Campaign 5",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "3300",
+    "campaign": "Campaign 5",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "800",
+    "campaign": "Campaign 5",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "3600",
+    "campaign": "Campaign 5",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "200",
+    "campaign": "Campaign 5",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 5",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 5",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "3700",
+    "campaign": "Campaign 6",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "9400",
+    "campaign": "Campaign 6",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "10300",
+    "campaign": "Campaign 6",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "6000",
+    "campaign": "Campaign 6",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "4800",
+    "campaign": "Campaign 6",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "2700",
+    "campaign": "Campaign 6",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "800",
+    "campaign": "Campaign 6",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "200",
+    "campaign": "Campaign 6",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 6",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 6",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "15900",
+    "campaign": "Campaign 7",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "18700",
+    "campaign": "Campaign 7",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "10800",
+    "campaign": "Campaign 7",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "8500",
+    "campaign": "Campaign 7",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "5500",
+    "campaign": "Campaign 7",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "800",
+    "campaign": "Campaign 7",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "5300",
+    "campaign": "Campaign 7",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "200",
+    "campaign": "Campaign 7",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 7",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 7",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "10300",
+    "campaign": "Campaign 8",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "12800",
+    "campaign": "Campaign 8",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "8300",
+    "campaign": "Campaign 8",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "7200",
+    "campaign": "Campaign 8",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "5100",
+    "campaign": "Campaign 8",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "600",
+    "campaign": "Campaign 8",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "3700",
+    "campaign": "Campaign 8",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "100",
+    "campaign": "Campaign 8",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "100",
+    "campaign": "Campaign 8",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 8",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "11200",
+    "campaign": "Campaign 9",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "13200",
+    "campaign": "Campaign 9",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "9900",
+    "campaign": "Campaign 9",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "7700",
+    "campaign": "Campaign 9",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "5200",
+    "campaign": "Campaign 9",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "1500",
+    "campaign": "Campaign 9",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "3800",
+    "campaign": "Campaign 9",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "900",
+    "campaign": "Campaign 9",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "500",
+    "campaign": "Campaign 9",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 9",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "9900",
+    "campaign": "Campaign 10",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "12100",
+    "campaign": "Campaign 10",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "8100",
+    "campaign": "Campaign 10",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "6700",
+    "campaign": "Campaign 10",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "4900",
+    "campaign": "Campaign 10",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "1500",
+    "campaign": "Campaign 10",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "3700",
+    "campaign": "Campaign 10",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "1000",
+    "campaign": "Campaign 10",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "700",
+    "campaign": "Campaign 10",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 10",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "7000",
+    "campaign": "Campaign 11",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "8900",
+    "campaign": "Campaign 11",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "6100",
+    "campaign": "Campaign 11",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "5100",
+    "campaign": "Campaign 11",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "3700",
+    "campaign": "Campaign 11",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "500",
+    "campaign": "Campaign 11",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "2500",
+    "campaign": "Campaign 11",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "300",
+    "campaign": "Campaign 11",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "200",
+    "campaign": "Campaign 11",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 11",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "5100",
+    "campaign": "Campaign 12",
+    "fico_band": "620-639"
+  },
+  {
+    "SUM_applications": "6400",
+    "campaign": "Campaign 12",
+    "fico_band": "640-659"
+  },
+  {
+    "SUM_applications": "4500",
+    "campaign": "Campaign 12",
+    "fico_band": "660-679"
+  },
+  {
+    "SUM_applications": "4300",
+    "campaign": "Campaign 12",
+    "fico_band": "680-699"
+  },
+  {
+    "SUM_applications": "3700",
+    "campaign": "Campaign 12",
+    "fico_band": "700-719"
+  },
+  {
+    "SUM_applications": "1800",
+    "campaign": "Campaign 12",
+    "fico_band": "600-619"
+  },
+  {
+    "SUM_applications": "1000",
+    "campaign": "Campaign 12",
+    "fico_band": "720-739"
+  },
+  {
+    "SUM_applications": "600",
+    "campaign": "Campaign 12",
+    "fico_band": "740-759"
+  },
+  {
+    "SUM_applications": "400",
+    "campaign": "Campaign 12",
+    "fico_band": "760-779"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 12",
+    "fico_band": "780-799"
+  },
+  {
+    "SUM_applications": "0",
+    "campaign": "Campaign 12",
+    "fico_band": "< 600"
+  }
+];
